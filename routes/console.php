84d89c417f77b06a6ecf42f050b3a48552118791
BinotaLIU <binota@binota.org>
2026-05-02 18:01:49 +0800

    chore: adjust task scheduling

--- routes/console.php
+++ routes/console.php
@@ -3,5 +3,18 @@
 use App\Console\Commands\FetchAnnouncementsCommand;
 use Illuminate\Support\Facades\Schedule;
 
+// NOTE: schedule_timezone is set to Asia/Taipei in config/app.php.
+
 Schedule::command(FetchAnnouncementsCommand::class)
-    ->everySixHours();
+    ->weekdays()
+    ->everyThirtyMinutes()
+    ->between('08:00', '22:00');
+
+Schedule::command(FetchAnnouncementsCommand::class)
+    ->weekdays()
+    ->everyTwoHours()
+    ->between('00:00', '08:00');
+
+Schedule::command(FetchAnnouncementsCommand::class)
+    ->weekends()
+    ->everyTwoHours();
