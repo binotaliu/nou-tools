621828c80b19de36048504e3f6b37c792160e4c2
BinotaLIU <binota@binota.org>
2026-09-21 16:54:21 +0800

    fix: wording

--- resources/views/schedule/print/_qr.blade.php
+++ resources/views/schedule/print/_qr.blade.php
@@ -6,6 +6,6 @@ of months leaves one, otherwise below the calendars. --}}
         {!! $page->qrCodeSvg !!}
     </div>
     <p class="text-[9pt] font-bold">掃描 QR Code，開啟線上課表</p>
-    <p class="text-[7.5pt] text-zinc-600">查看每堂課的時間，並進入視訊教室上課</p>
+    <p class="text-[7.5pt] text-zinc-600">檢視每堂課的時間，並進入視訊教室上課</p>
     <p class="max-w-[80mm] text-[6.5pt] break-all text-zinc-500">{{ $page->shareUrl }}</p>
 </div>
